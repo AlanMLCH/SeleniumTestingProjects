58df9aec580d93471708069fa9914abd11109c06
Alan Martín López Chávez <alan.m.l.ch.1997@gmail.com>
2023-01-05 01:43:32 -0600

    chai

--- SeleniumTraining/index.js
+++ SeleniumTraining/index.js
@@ -1,26 +1,42 @@
 const {By,Key,Builder} = require("selenium-webdriver");
-require("chromedriver");
+const assert = require("assert");
+// const chromeD = require("chromedriver");
+// const should = require("chai").should();
  
-async function example(){
- 
+async function example(url, browser){
+        //Some variables
        let searchString = "Automation testing with Selenium";
  
-       //To wait for browser to build and launch properly
-       let driver = await new Builder().forBrowser("chrome").build();
+       //Launch Browser
+       let driver = await new Builder().forBrowser(browser).build();
  
-        //To fetch http://google.com from the browser with our code.
-        await driver.get("http://google.com");
+        //To fetch some url.
+        await driver.get(url);
             
         //To send a search query by passing the value in searchString.
-        await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
- 
-        //Verify the page title and print it
+        // await driver.findElement(By.css('.form-control[name="my-textarea"]')).sendKeys(searchString,Key.RETURN);
+
+        //assertion
+
+        let todoText = await driver.findElement(By.css('[name="my-select"] [selected]')).getText().then(function(value){
+                return value
+        });
+      
+        //assertion Node
+
+        assert.strictEqual(todoText, "Three")
+
+        //assert using chai should
+        // todoText.should.equal("Three");
+
+        //Something to do 
+        await new Promise(res => setTimeout(res, 10000));
         let title = await driver.getTitle();
-        console.log('Title is:',title);
+        console.log('Title is:',todoText);
  
-        //It is always a safe practice to quit the browser after execution
+        //Close the browser
         await driver.quit();
  
 }
  
-example()
+example('https://www.selenium.dev/selenium/web/web-form.html', 'chrome')
