f363950eac382cb82d86468dc5e49bdbfa5cf256
Alan Martín López Chávez <alan.m.l.ch.1997@gmail.com>
2023-01-12 01:25:42 -0600

    more tests

--- SeleniumTraining/test/test3.js
+++ SeleniumTraining/test/test3.js
@@ -50,6 +50,44 @@ describe("add todo tests", function(){
                 
         });
 
+        it("succesfully adds a todo application", async function(){
+                //Launch Browser
+                let driver = await new Builder().forBrowser("chrome").build();
+        
+                //To fetch some url.
+                await driver?.get("https://www.selenium.dev/selenium/web/web-form.html");
+                
+                // To send a search query by passing the value in searchString.
+                await driver
+                ?.findElement(By?.css('.form-control[name="my-textarea"]'))
+                ?.sendKeys(searchString,Key.RETURN);
+
+                //assertion
+
+                let todoText = await driver
+                ?.findElement(By?.css('[name="my-select"] [selected]'))
+                ?.getText()
+                .then(function(value){
+                        return value
+                });
+        
+                // //assertion Node
+
+                // assert.strictEqual(todoText, "Three")
+
+                //assert using chai should
+                todoText.should.equal("Three");
+
+                //Something to do 
+                await new Promise(res => setTimeout(res, 10000));
+                let title = await driver.getTitle();
+                console.log('Title is:',todoText);
+        
+                //Close the browser
+                await driver.quit();
+                
+        });
+
 });
 
  
